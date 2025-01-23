c948331e77cee966809c9ac1cdc65deb6aa522c3
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-23 08:58:20 -0700

    started program

--- SimpleCalculator/SimpleCalculator.vb
+++ SimpleCalculator/SimpleCalculator.vb
@@ -1,7 +1,12 @@
-﻿Module SimpleCalculator
+﻿Imports System.IO
+
+Module SimpleCalculator
 
     Sub Main()
-
+        Dim JellyBeans As Integer
+        Console.WriteLine(" Input numbers and + or - ")
+        JellyBeans = Console.Read
+        Console.Write(JellyBeans)
     End Sub
 
 End Module
