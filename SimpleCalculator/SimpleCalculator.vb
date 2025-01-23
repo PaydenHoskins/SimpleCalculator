dd97fb843957e6c6789cd5d7857ecbe0eecf1c88
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-23 09:08:39 -0700

    more operations

--- SimpleCalculator/SimpleCalculator.vb
+++ SimpleCalculator/SimpleCalculator.vb
@@ -4,9 +4,12 @@ Module SimpleCalculator
 
     Sub Main()
         Dim JellyBeans As Integer
+        Dim FootBall As Integer
         Console.WriteLine(" Input numbers and + or - ")
         JellyBeans = Console.Read
-        Console.Write(JellyBeans)
+        Console.ReadLine()
+        FootBall = Console.Read
+        Console.WriteLine(JellyBeans + FootBall)
     End Sub
 
 End Module
