2adb3b9064a09f9d47934eb80c278696e0abf996
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-23 16:54:09 -0700

    read line operations

--- SimpleCalculator/SimpleCalculator.vb
+++ SimpleCalculator/SimpleCalculator.vb
@@ -5,11 +5,13 @@ Module SimpleCalculator
     Sub Main()
         Dim JellyBeans As Integer
         Dim FootBall As Integer
-        Console.WriteLine(" Input numbers and + or - ")
-        JellyBeans = Console.Read
-        Console.ReadLine()
-        FootBall = Console.Read
-        Console.WriteLine(JellyBeans + FootBall)
+        Dim operation As String
+        Console.WriteLine(" number enter operation enter number enter ")
+        JellyBeans = Console.Read()
+        operation = Console.ReadLine()
+        FootBall = Console.Read()
+        Console.WriteLine()
+        Console.Write(JellyBeans & operation & FootBall)
     End Sub
 
 End Module
