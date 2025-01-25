d6089fbbfd2369c30c947ea621e178212ae135a3
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-24 20:45:42 -0700

    remove old none working code

--- SimpleCalculator/SimpleCalculator.vb
+++ SimpleCalculator/SimpleCalculator.vb
@@ -16,11 +16,7 @@ Module SimpleCalculator
         Dim secondNumber As Integer
         Dim operation As String
         Console.WriteLine(" number enter operation enter number enter ")
-        JellyBeans = Console.Read()
-        operation = Console.ReadLine()
-        FootBall = Console.Read()
-        Console.WriteLine()
-        Console.Write(JellyBeans & operation & FootBall)
+
     End Sub
 
 End Module
